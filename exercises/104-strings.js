c9b1da4eb18f9c41415a737d604d5bc80265a3c7
gauraklein <gaurakklein@gmail.com>
2019-06-22 13:46:39 -0500

    no errors

--- exercises/104-strings.js
+++ exercises/104-strings.js
@@ -5,6 +5,9 @@
 // 1) Create a function "helloWorld"
 // 2) Return the string "Hello, world!"
 
+function helloWorld() {
+  return "Hello, world!";
+}
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 1) Create a function "helloName" that accepts 1 parameter (arity of 1)
@@ -12,7 +15,7 @@
 // 3) Return the string "Hello, <name>!" where <name> is the value passed to the function
 
 function helloName (name) {
-
+    return "Hello, " + name + "!";
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -26,6 +29,7 @@ function abstractLength () {
     'We identify common causes of complexity and discuss general approaches which can be taken to eliminate them where they are accidental in nature. ' +
     'To make things more concrete we then give an outline for a potential complexity-minimizing approach based on functional programming and Codd’s relational model of data.'
 
+    return tarPitAbstract.length;
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -35,7 +39,7 @@ function abstractLength () {
 
 function makeLoud () {
   const chorus = 'Who let the dogs out?'
-
+  return chorus.toUpperCase();
 }
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -45,5 +49,5 @@ function makeLoud () {
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toLowerCase
 
 function makeQuiet (str) {
-
+  return str.toLowerCase();
 }
